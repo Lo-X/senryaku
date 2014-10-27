5d076350ce0a75d6ade907755b0b3e3381b2f558
Lo-X <loic.boutter@gmail.com>
2014-10-27 16:02:39 +0100

    Adding a body to GameState class

--- states/gamestate.cpp
+++ states/gamestate.cpp
@@ -1,5 +1,21 @@
 #include <states/gamestate.hpp>
 
-GameState::GameState()
+GameState::GameState(StateStack &stack, Context context) :
+    State(stack, context)
 {
+}
+
+
+void GameState::draw()
+{
+}
+
+bool GameState::update(sf::Time dt)
+{
+    return true;
+}
+
+bool GameState::handleEvent(const sf::Event& event)
+{
+    return true;
 }
--- states/gamestate.hpp
+++ states/gamestate.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+
 #include <states/state.hpp>
 
 class GameState : public State
 {
 public:
-    GameState();
+    GameState(StateStack& stack, Context context);
+
+    virtual void        draw();
+    virtual bool        update(sf::Time dt);
+    virtual bool        handleEvent(const sf::Event &event);
 };
 
