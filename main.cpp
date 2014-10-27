ec8995f8ecec9e04ec1e143a64a4658698701d9f
Lo-X <loic.boutter@gmail.com>
2014-10-27 16:33:37 +0100

    Exception proof main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <application/application.hpp>
-
+#include <iostream>
 
 int main()
 {
-    Application app(1024, 768, "Senryaku");
-    app.run();
+    try
+    {
+        Application app(1024, 768, "Senryaku");
+        app.run();
+    }
+    catch(std::exception& e)
+    {
+        std::cerr << std::endl << "Senryaku crashed on exception: " << e.what() << std::endl;
+    }
 
     return 0;
 }
